ad2cc272e7f209e39b206c315e6f9358c9f8994e
Luca <lucakanyo@protonmail.com>
2020-08-27 14:21:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(username, useremail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json", 
+            "Accept": "application/json"
+        }, 
+        body: JSON.stringify({
+            name: username, 
+            email: useremail
+        })
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        let p = document.createElement('p')
+        p.innerHTML = object.id 
+        document.body.appendChild(p)
+        console.log(object);
+    })
+    .catch(function(error){
+        //alert("nope");
+        let errorp = document.createElement('p')
+        errorp.innerHTML = error.message 
+        document.body.appendChild(errorp)
+        //console.log(error.message);
+    });
+}
+

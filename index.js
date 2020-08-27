bad9ee7628826062e29b737860dd26e7d9909e56
charlie763 <cwisoff@gmail.com>
2020-08-27 15:38:56 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+
+function submitData(username, email){
+  const userData = {
+    name: username,
+    email: email
+  };
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  };
+
+  return fetch(`http://localhost:3000/users`, configObj)
+    .then(response => response.json())
+    .then(json => displayId(json))
+    .catch((error)=>{
+      document.body.innerHTML += `${error.message}`
+    });
+}
+
+function displayId(json){
+  document.body.innerHTML += `${json.id}`
+}

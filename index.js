3db5067679df4c3ed8977a4ff5f919b09cea8ee3
KaelinAlexander <kaelin.alexander@gmail.com>
2020-09-01 10:54:53 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+
+function submitData(usernameInput, useremailInput) {
+
+    let formData = {
+        name: usernameInput,
+        email: useremailInput
+      };
+       
+
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+        let target = document.body.createElement('h1')
+        target.innerHTML = (object.id);
+    })
+    .catch(function(error) {
+        alert("Bad things! Ragnarok!");
+        let target = document.body.appendChild('h1')
+        target.innerHTML = (error.message);
+    })
+
+}

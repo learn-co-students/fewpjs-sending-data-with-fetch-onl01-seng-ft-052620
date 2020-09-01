e6def51dd183d3d3a6035164b3fcce779d98d1dd
shazy89 <erdo.shazy123@gmail.com>
2020-09-01 12:29:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+const url = "http://localhost:3000"
+
+function submitData( name, email ) {
+    return fetch( url + '/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }

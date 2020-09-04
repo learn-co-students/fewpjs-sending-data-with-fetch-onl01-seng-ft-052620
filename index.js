9212d5ae3b9ef2a4d94d01f40a013f88c6e6c750
github username <github email address>
2020-09-04 15:47:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email){
+    let data = {
+        name: name,
+        email: email
+    };
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+        body: JSON.stringify(data)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(resp => resp.json())
+    .then(function(obj){
+        document.querySelector("body").append(obj.id);
+    })
+    .catch(function(error){
+        document.querySelector("body").append(error.message);
+    })
+}

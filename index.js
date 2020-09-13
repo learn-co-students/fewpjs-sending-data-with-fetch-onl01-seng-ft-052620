1dc1b5a0b82d7d488ae228d75cf50dce69d39de4
ejguzman3988 <eriberto.guzman@outlook.com>
2020-09-13 03:57:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+
+function submitData(n, e){
+    let formData = {
+        name: n,
+        email: e
+    }
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+    return fetch("http://localhost:3000/users", configObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        let el = document.createElement('p')
+        el.innerHTML = object.name;
+        let li = document.createElement('li')
+        li.innerHTML = object.email;
+        let li2 = document.createElement('li')
+        li2.innerHTML = object.id
+        document.body.appendChild(el)
+        el.appendChild(li)
+        el.appendChild(li2)
+        
+      })
+      .catch(function(error) {
+        let el = document.createElement('p')
+        el.innerHTML = error.message
+        document.body.appendChild(el)
+      });
+}
+
+
+// user name
+// email

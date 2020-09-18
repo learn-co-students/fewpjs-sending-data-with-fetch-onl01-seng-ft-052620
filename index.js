c3ecd4991dffc94c298ba1509b00f1ab0995de32
chibuisi <enyia21@gmail.com>
2020-09-18 18:23:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const destinationURL = 'http://localhost:3000/users'
+    const configrationObj = {
+        method: "Post",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          name: userName,
+          email: userEmail
+        })
+    }
+    return fetch(destinationURL, configrationObj)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        console.log(object)
+    })
+
+}
+
+// function appendToDOM(object){
+//     console.log(object);
+// }
